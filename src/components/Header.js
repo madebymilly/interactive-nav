31b14914222c546c84746dc68af80e6e3ea48a30
Milly Uylenbroek <m.uylenbroek@eagerly.nl>
2022-02-01 19:57:17 +0100

    show nav on click hamburger

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,36 +5,47 @@ import useScrollingDown from '../hooks/useScrollingDown';
 
 function Header() {
 
-  const scrolledUp = useScrollingUp()
-  const scrolledDown = useScrollingDown()
-
   const [headerHeight, setHeaderHeight] = useState(0);
-  const headerRef = useRef(null)
+  const [isNavOpen, toggleIsNavOpen] = useState(false); // HOOK MAKEN
+
+  const headerRef = useRef(null);
 
   useEffect(() => {
     setHeaderHeight(headerRef.current.clientHeight);
   }, []);
 
+  const handleClick = () => {
+    console.log('click')
+    toggleIsNavOpen(!isNavOpen);
+  }
+
   const { ref, inView, entry } = useInView({
     rootMargin: '-' + headerHeight + 'px'
   });
 
-  let up = false;
-  let down = false;
+  const scrolledUp = useScrollingUp();
+  const scrolledDown = useScrollingDown();
 
-  if (!inView && scrolledUp) {
-    up = true;
-  } else if (!inView && scrolledDown) {
-    down = true;
+  let isHeaderDark = false;
+  let isHeaderHidden = false;
+
+  if ( !inView ) {
+    if ( scrolledUp || isNavOpen ) {
+      isHeaderDark = true;
+    } else if ( scrolledDown ) {
+      isHeaderHidden = true;
+    }
+  } else {
+    isHeaderDark = false;
   }
 
   return (
     <header className="header">
       <div 
-        className={`header__nav-bar ${up ? 'header__nav-bar--dark' : ''} ${down ? 'header__nav-bar--hidden' : ''}`} 
+        className={`header__nav-bar ${isHeaderDark && 'header__nav-bar--dark'} ${isHeaderHidden && 'header__nav-bar--hidden'}`} 
         ref={headerRef}
       >
-        <a href="/" className="header__logo">
+        <a href="/" className={`header__logo ${isHeaderHidden && 'header__logo--dark'}`}>
           KALIBER
         </a>
 
@@ -48,8 +59,10 @@ function Header() {
           </nav>
         </div>
 
-        <button className="header__button">
-          <span className="header__icon">&nbsp;</span>
+        <button 
+          className={`header__button ${isHeaderHidden && 'header__button--shown header__button--fixed'} ${isHeaderDark && 'header__button--shown'}`} 
+          onClick={handleClick}>
+          <span className={`header__icon ${isNavOpen && 'header__icon--close'}`}>&nbsp;</span>
         </button>
         
       </div>
