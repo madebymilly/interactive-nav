099d6773a3eb70cbbb7cc27e9497848c6cfb0684
Milly Uylenbroek <m.uylenbroek@eagerly.nl>
2022-02-07 16:11:23 +0100

    CSS Module for MenuButton with classnames

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import classnames from 'classnames';
 import styles from '../sass/MenuButton.module.scss';
 
 export default function MenuButton(props) {
   const { handleClick, handleClickMobile, isHeaderDark, isHeaderHidden, isMobileNavOpen } = props;
+  const menuBtnClasses = classnames(styles.menuButton, {
+    [styles.menuButtonShown]: isHeaderHidden || isHeaderDark,
+    [styles.menuButtonFixed]: isHeaderHidden
+  });
   return (
     <>
       <button 
-          className={`${styles.menuButton} header__button ${isHeaderHidden ? 'header__button--shown header__button--fixed' : ''} ${isHeaderDark ? 'header__button--shown' : ''}`} 
+          className={menuBtnClasses} 
           onClick={handleClick}>
           <span className={`header__icon ${!isHeaderHidden && isHeaderDark ? 'header__icon--close' : ''}`}>&nbsp;</span>
         </button>
 
         <button 
-          className={`${styles.menuButton} header__button header__button--mobile`}
+          className={`${styles.menuButton} ${styles.menuButtonMobile}`}
           onClick={handleClickMobile}>
           <span className={`header__icon ${isMobileNavOpen ? 'header__icon--close' : ''}`}>&nbsp;</span>
         </button>
